db16bf7b0b88869760003fd7daa545bbd5f74961
bhumi46 <thisisbn46@gmail.com>
2024-08-01 23:05:37 +0530

    target
    Signed-off-by: bhumi46 <thisisbn46@gmail.com>

--- MockMDS/updating-app-properties.sh
+++ MockMDS/updating-app-properties.sh
@@ -17,7 +17,7 @@ fi
 pwd
 
 # Define the properties file path
-PROPERTIES_FILE="application.properties"
+PROPERTIES_FILE="/target/application.properties"
 
 # Check if the properties file exists
 if [ ! -f "$PROPERTIES_FILE" ]; then
