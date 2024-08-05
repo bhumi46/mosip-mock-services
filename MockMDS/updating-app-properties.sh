0ed776cfd5ee48829d1536497c1166286b5b5417
bhumi46 <thisisbn46@gmail.com>
2024-08-05 15:10:10 +0530

    updatd .sh
    Signed-off-by: bhumi46 <thisisbn46@gmail.com>

--- MockMDS/updating-app-properties.sh
+++ MockMDS/updating-app-properties.sh
@@ -20,7 +20,7 @@ PROPERTIES_FILE="target/application.properties"
 # Update the placeholders in the application.properties file
 sed -i "s|\$API_INTERNAL_HOST|$API_INTERNAL_HOST|g" $PROPERTIES_FILE
 sed -i "s|\$mosip_regproc_client_secret|$mosip_regproc_client_secret|g" $PROPERTIES_FILE
-sed -i "s|\$KEYSTORE_PWD|$keystore_pwd|g" $PROPERTIES_FILE
+sed -i "s|\$keystore_pwd|$keystore_pwd|g" $PROPERTIES_FILE
 
 echo "application.properties updated successfully."
 
